22e30d7f7f02d4718b3aebade43d95b3bdc7e1ec
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2021-11-03 13:30:59 +0800

    front page partial
    -adding frames for php
    -pics
    -partial full front page

--- frame2.php
+++ frame2.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cut 'n Fold Catering Services</title>
+    <title>Book</title>
 </head>
 <body>
+    <div id="logo">
+
+    </div>
+        //pic
+    <div>
+
+    </div>
     
 </body>
 </html>
